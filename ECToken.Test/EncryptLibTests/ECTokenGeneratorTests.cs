ab6660442d85a5cbccecebc68f391104b690a2d5
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-09-08 18:28:39 +0200

    cleanuy

--- ECToken.Test/EncryptLibTests/ECTokenGeneratorTests.cs
+++ ECToken.Test/EncryptLibTests/ECTokenGeneratorTests.cs
@@ -12,82 +12,44 @@
         public void EncryptV3_WithDateTimeAndClientIP_ReturnsEcnryptedTokenWithBoth()
         {
             //arrange
-            var generator = new ECTokenGenerator();
+
             var expireTime = DateTime.Now.AddMilliseconds(300);
             string clientIp = Faker.Internet.IPv4Address();
-            string key = Faker.Name.FullName();
+            ECTokenGenerator.Key key = new (Faker.Name.FullName());
 
             //act
-            var token = generator.EncryptV3(key, expireTime, clientIp);
+            var token = ECTokenGenerator.EncryptV3(
+                key: key, 
+                expirationTime: expireTime, 
+                clientIPAddress: clientIp);
 
             //assert
 
             Assert.NotNull(token);
-            var decryptdToken = generator.DecryptV3(key, token, false);
+            var decryptdToken = token.DecryptV3(key);
 
             string expected = $"ec_expire={expireTime.FromEpoch()}&ec_clientip={clientIp}";
-            Assert.Equal(expected, decryptdToken);
+            Assert.Equal(expected, decryptdToken.Value);
         }
 
         [Fact]
         public void EncryptV3_WithDateTimeOnly_ReturnsEncryptedTokenWithOnlyDate()
         {
             //arrange
-            var generator = new ECTokenGenerator();
             var expireTime = DateTime.Now.AddMilliseconds(300);
-            string key = Faker.Name.FullName();
+            ECTokenGenerator.Key key = new (Faker.Name.FullName());
 
             //act
-            var token = generator.EncryptV3(key, expireTime);
+            var token = ECTokenGenerator.EncryptV3(
+                key: key,
+                expirationTime: expireTime);
 
             //assert
             Assert.NotNull(token);
-            var decryptdToken = generator.DecryptV3(key, token, false);
+            var decryptdToken = token.DecryptV3(key);
 
             string expected = $"ec_expire={expireTime.FromEpoch()}";
-            Assert.Equal(expected, decryptdToken);
-        }
-
-        [Fact]
-        public void NextRandomString_WithLength_ReturnsStringWithSpecifiedSize()
-        {
-            //arrange
-            var generator = new ECTokenGenerator();
-            int length = 50;
-
-            //act
-            var random = generator.NextRandomString(50);
-
-            //assert
-            Assert.Equal(length, random.Length);
-        }
-
-        [Fact]
-        public void NextRandomString_WithNoLength_ReturnsStringBetweenMINAndMAX()
-        {
-            //arrange
-            var generator = new ECTokenGenerator();
-            int lengthMin = 4;
-            int lengthMax = 8;
-            //act
-            var random = generator.NextRandomString();
-
-            //assert
-            Assert.True(random.Length >= lengthMin && random.Length <= lengthMax);
-        }
-
-
-        [Fact]
-        public void NextRandomString_ReturnsString()
-        {
-            //arrange
-            var generator = new ECTokenGenerator();
-
-            //act
-            var random = generator.NextRandomString();
-
-            //assert
-            Assert.IsType<string>(random);
+            Assert.Equal(expected, decryptdToken.Value);
         }
     }
 }
--- ec_encryptcore/Program.cs
+++ ec_encryptcore/Program.cs
@@ -1,116 +1,47 @@
-﻿using System;
-using ecencryptstdlib;
-
-namespace ec_encryptcore
+﻿namespace ec_encryptcore
 {
+    using System;
+    using ecencryptstdlib;
+
     class Program
     {
-        private static readonly string DECRYPT_COMMAND = "decrypt";
-        private static readonly string VERBOSE_COMMAND = "-v";
-
         static void Main(string[] args)
         {
             try
             {
-                ECTokenGenerator tokenGenerator = new ECTokenGenerator();
-
                 if (args.Length == 1)
                 {
-
-                    System.Console.WriteLine("EC Token encryption and decryption utility.  Version: 3.0.0");
-                    System.Console.WriteLine(".NET Core Version supported by ecencryptstdlib (.NET Standard 1.4) ");
+                    Console.WriteLine("EC Token encryption and decryption utility.  Version: 3.0.0");
+                    Console.WriteLine(".NET Core Version supported by ecencryptstdlib (.NET Standard 1.4) ");
                     Environment.Exit(0);
                 }
 
                 if (args.Length < 2)
                 {
                     // display some examples of how to use this application
-                    System.Console.WriteLine("----------------------------------------------------------------");
-                    System.Console.WriteLine("| Usage / Help:");
-                    System.Console.WriteLine("|       ec_encryptcore.exe <key> <text>             | create v3 encrypt token using <text> and <key>");
-                    System.Console.WriteLine("|       ec_encryptcore.exe <key> <text> -v          | create v3 encrypt token using <text> and <key> in verbose mode");
-                    System.Console.WriteLine("|       ec_encryptcore.exe decrypt <key> <text>     | decrypt token");
-                    System.Console.WriteLine("|       ec_encryptcore.exe decrypt <key> <text> -v  | decrypt token in verbose mode");
-                    System.Console.WriteLine("---------------------------------------------------------------");
+                    Console.WriteLine("----------------------------------------------------------------");
+                    Console.WriteLine("| Usage / Help:");
+                    Console.WriteLine("|       ec_encryptcore.exe <key> <text>             | create v3 encrypt token using <text> and <key>");
+                    Console.WriteLine("|       ec_encryptcore.exe decrypt <key> <text>     | decrypt token");
+                    Console.WriteLine("---------------------------------------------------------------");
                     Environment.Exit(1);
                 }
 
-                // variables to store the key and token
-                string strKey = "";
-                string strToken = "";
-                string strResult = "";
-                bool isEncrypt = true;
+                bool isEncrypt = args[0] != "decrypt";
 
-                // we can turn on verbose output to help debug problems
-                bool blnVerbose = false;
+                var (key, strToken) = isEncrypt 
+                    ? (new ECTokenGenerator.Key(args[0]), new ECTokenGenerator.Token(args[1])) 
+                    : (new ECTokenGenerator.Key(args[1]), new ECTokenGenerator.Token(args[2]));
 
-                if (blnVerbose) System.Console.WriteLine("----------------------------------------------------------------\n");
+                var result = isEncrypt
+                    ? strToken.EncryptV3(key)
+                    : strToken.DecryptV3(key);
 
-                if (args.Length > 2) { if (args[2] == VERBOSE_COMMAND) blnVerbose = true; }
-                if (args.Length > 3) { if (args[3] == VERBOSE_COMMAND) blnVerbose = true; }
-
-                if (args[0] == DECRYPT_COMMAND) isEncrypt = false;
-
-                // if this is a decrypt function, then take an encrypted token and decrypt it
-                if (isEncrypt)
-                {
-                    strKey = args[0];
-                    strToken = args[1];
-
-                    try
-                    {
-                        strResult = tokenGenerator.EncryptV3(strKey, strToken, blnVerbose);
-
-                        if (string.IsNullOrEmpty(strResult))
-                            System.Console.WriteLine("Failed to encrypt token");
-                    }
-                    catch (System.Exception ex)
-                    {
-                        if (blnVerbose)
-                        {
-                            System.Console.WriteLine("Exception occured while encrypting token" + ex.Message);
-                            Environment.Exit(1);
-                        }
-                    }
-                }
-                else
-                {
-                    strKey = args[1];
-                    strToken = args[2];
-
-                    try
-                    {
-                        strResult = tokenGenerator.DecryptV3(strKey, strToken, blnVerbose);
-                        if (string.IsNullOrEmpty(strResult))
-                        {
-                            System.Console.WriteLine("Failed to decrypt token.");
-                        }
-                    }
-                    catch (System.Exception ex)
-                    {
-                        if (blnVerbose)
-                            System.Console.WriteLine("Exception occured while encrypting token" + ex.Message);
-                    }
-                }
-
-                if (blnVerbose)
-                {
-                    System.Console.WriteLine("----------------------------------------------------------------");
-                }
-
-                if (!string.IsNullOrEmpty(strResult))
-                {
-                    System.Console.WriteLine(strResult);
-                }
-                else
-                {
-                    System.Console.WriteLine("Failed to encrypt/decrypt token");
-                    Environment.Exit(1);
-                }
+                Console.WriteLine(result);
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
-                System.Console.WriteLine("Exception occured while encrypting/decrypting token" + ex.Message);
+                Console.Error.WriteLine($"Exception occured while encrypting/decrypting token: {ex.Message}");
             }
         }
     }
